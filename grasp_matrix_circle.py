17a6de75624864e81f6502e4a8a6335e85e84028
Kush Patel <97977579+kushpatel19@users.noreply.github.com>
2024-07-06 01:20:35 +0530

    Update grasp_matrix_circle.py

--- grasp_matrix_circle.py
+++ grasp_matrix_circle.py
@@ -8,19 +8,6 @@ Grasp Matrix Calculator
 This script calculates the grasp matrix for a multi-fingered robot hand
 holding a circle at specified contact points in 2D/3D.
 
-Copyright 2023 Kush Patel
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 '''
 # Importing necessary libraries
 import numpy as np
