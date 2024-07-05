dac0feef97b0469d1b4cf358c9b5a7619646c064
Kush Patel <97977579+kushpatel19@users.noreply.github.com>
2024-07-06 01:19:30 +0530

    Update Grasp_Matrix.py

--- Grasp_Matrix.py
+++ Grasp_Matrix.py
@@ -9,20 +9,6 @@ Grasp Matrix Calculator
 This script calculates the grasp matrix for a multi-fingered robot hand
 holding an object with a given surface equation of any random obejct at specified contact points.
 It can visualize the object's surface and contact points in 3D.
-
-Copyright 2023 Kush Patel
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 '''
 
 import numpy as np
