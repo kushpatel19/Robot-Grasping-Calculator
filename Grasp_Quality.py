86a11045c4a5ff8408f424a7e6a3d47b4c12cd27
Kush Patel <97977579+kushpatel19@users.noreply.github.com>
2024-07-06 01:19:45 +0530

    Update Grasp_Quality.py

--- Grasp_Quality.py
+++ Grasp_Quality.py
@@ -9,20 +9,6 @@ This script evaluates the grasp quality for a multi-fingered robot hand
 holding a circular object at specified contact points in 2D/3D. It calculates
 four grasp quality metrics: Q_MSV(G), Q_EV(G), Q_GI(G), & Q_GPRI() and finds the best finger
 configurations according to each of quality measures.
-
-Copyright 2023 Kush Patel
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 '''
 # Import necessary libraries
 import numpy as np
