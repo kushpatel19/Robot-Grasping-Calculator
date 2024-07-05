e2408b812492bf807d37287f59b474ba35d5baff
Kush Patel <97977579+kushpatel19@users.noreply.github.com>
2024-07-06 01:20:17 +0530

    Update Hand_Jacobian.py

--- Hand_Jacobian.py
+++ Hand_Jacobian.py
@@ -9,20 +9,6 @@ Hand Jacobian Calculator
 This script calculates the hand Jacobian matrix for a multi-fingered robot hand holding an object. 
 The robot hand is a serial chain robotic manipulator with specified joint angles and contact positions on the object's surface.
 The script computes the hand Jacobian to describe the relationship between joint velocities and the linear and angular velocities of the object.
-
-Copyright 2023 Kush Patel
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 '''
 
 import numpy as np
